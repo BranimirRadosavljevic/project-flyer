c997a47328e36a44bac4c2ac17aca420835f1eee
Branimir Radosavljevic <rbranimir@ymail.com>
2020-05-22 17:17:43 +0200

    Episode 5

--- resources/views/flyers/create.blade.php
+++ resources/views/flyers/create.blade.php
@@ -2,4 +2,10 @@
 
 @section('content')
     <h1>Selling Your Home?</h1>
+
+    <hr>
+
+    <form action="/flyers" method="POST" enctype="multipart/form-data">
+        @include('flyers.form')
+    </form>
 @endsection
