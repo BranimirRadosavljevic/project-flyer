620e21748397ebecc84df3b4291ff1487fc324af
Branimir Radosavljevic <rbranimir@ymail.com>
2020-05-23 23:17:07 +0200

    Episode 10

--- app/Flyer.php
+++ app/Flyer.php
@@ -8,8 +8,21 @@ class Flyer extends Model
 {
     protected $guarded = [];
 
+    public function scopeLocatedAt($query, $zip, $street)
+    {
+        $street = str_replace('-', ' ', $street);
+        return $query->where(compact('zip', 'street'));
+    }
+
+    public function getPriceAttribute($price)
+    {
+        return '$' . number_format($price);
+    }
+
     public function photos()
     {
         return $this->hasMany(Photo::class);
     }
+
+
 }
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,4 @@ Route::get('/', function () {
 });
 
 Route::resource('flyers', 'FlyersController');
+Route::get('{zip}/{street}', 'FlyersController@show');
