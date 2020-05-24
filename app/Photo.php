49729d3f1c212cbda13f54b2828b23b9aca76313
Branimir Radosavljevic <rbranimir@ymail.com>
2020-05-24 17:23:39 +0200

    Episode 13

--- app/Photo.php
+++ app/Photo.php
@@ -4,23 +4,44 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\UploadedFile;
+use Intervention\Image\Facades\Image;
 
 class Photo extends Model
 {
     protected $table = 'flyer_photos';
 
-    protected $fillable = ['path'];
+    protected $fillable = ['name', 'path', 'thumbnail_path'];
 
     protected $baseDir = 'images';
 
 
-    public static function fromForm(UploadedFile $file)
+    public static function named($name)
     {
         $photo = new static;
-        $name = time() . $file->getClientOriginalName();
-        $photo->path = asset($photo->baseDir . '/' . $name);
-        $file->move($photo->baseDir, $name);
-        return $photo;
+        return $photo->saveAs($name);
+    }
+
+    public function saveAs($name)
+    {
+        $this->name = sprintf("%s-%s", time(), $name);
+        $this->path = asset(sprintf("%s/%s", $this->baseDir, $this->name));
+        $this->thumbnail_path = asset(sprintf("%s/tn-%s", $this->baseDir, $this->name));
+
+        return $this;
+    }
+    
+    public function move(UploadedFile $file)
+    {
+        $file->move($this->baseDir, $this->name);
+
+        $this->makeThumbnail();
+
+        return $this;
+    }
+
+    protected function makeThumbnail()
+    {
+        Image::make(sprintf("%s/%s", $this->baseDir, $this->name))->fit(200)->save(sprintf("%s/tn-%s", $this->baseDir, $this->name));
     }
 
     public function flyer()
